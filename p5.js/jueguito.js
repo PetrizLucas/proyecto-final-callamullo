3f2006f7c1c637d7cd22d93e98bbd8d017e5ad4a
PetrizLucas <109303293+PetrizLucas@users.noreply.github.com>
2022-09-08 18:42:43 +0200

    Add files via upload

---
+++ p5.js/jueguito.js
@@ -0,0 +1,13 @@
+// Single-sketch example
+
+function setup (){
+  createCanvas (800, 600);
+}
+
+function draw(){
+  background(100);
+  fill(255);
+  noStroke();
+trianglemode(center)
+  rectangle(mouseX, mouseY, 80, 20,20,20);
+}
